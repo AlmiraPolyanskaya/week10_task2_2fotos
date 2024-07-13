6e2ae13cb3e15ebf4f4f28ec2d6b908221b17a8a
AlmiraPolyanskaya <akubagusheva@gmail.com>
2024-07-13 20:58:17 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let image = document.getElementById('image');
+
+function previousFoto() {
+    image.src = './assets/images/foto1.jpg';
+}
+
+function nextFoto() {
+    image.src = './assets/images/foto2.jpeg';
+}
